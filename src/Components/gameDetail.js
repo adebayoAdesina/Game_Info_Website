d85cfe233cc4c3dd7611dd91c446fb2970b71883
adebayoAdesina <adebayoadesina789@gmail.com>
2022-09-27 09:01:55 +0100

    star rating icon added.

--- src/Components/gameDetail.js
+++ src/Components/gameDetail.js
@@ -10,6 +10,8 @@ import xbox from "../img/xbox.svg";
 import nintendo from "../img/nintendo.svg";
 import apple from "../img/apple.svg";
 import gamepad from "../img/gamepad.svg";
+import starEmpty from "../img/star-empty.png";
+import starFull from "../img/star-full.png";
 
 const Gamedetail = ({ pathId }) => {
   const navigation = useNavigate();
@@ -41,6 +43,20 @@ const Gamedetail = ({ pathId }) => {
     }
   };
 
+  const getStars = (e) => {
+    const stars = [];
+    const rating = Math.floor(e);
+    for (var i = 1; i <= 5; i++) {
+      if (i <= rating) {
+        stars.push(<img src={starFull} alt="stars" />);
+      } else {
+        stars.push(<img src={starEmpty} alt="stars" />);
+      }
+
+      console.log(stars);
+    }
+    return stars;
+  };
   return (
     <>
       {!isLoading && (
@@ -50,6 +66,7 @@ const Gamedetail = ({ pathId }) => {
               <div className="rating">
                 <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
                 <p>Rating: {game.rating}</p>
+                {getStars(game.rating)}
               </div>
               <Info>
                 <h3>Platforms</h3>
@@ -126,6 +143,11 @@ const Stats = styled(motion.div)`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  img {
+    width: 2rem;
+    height: 2rem;
+    display: inline;
+  }
 `;
 
 const Info = styled(motion.div)`
